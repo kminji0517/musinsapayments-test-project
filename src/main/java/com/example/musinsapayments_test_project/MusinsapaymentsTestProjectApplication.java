24f31d041b420c264937e97620d90b23e23d57b6
kminji0517 <kminji0517@gmail.com>
2026-05-07 22:17:00 +0900

    init: 프로젝트 초기 설정

---
+++ src/main/java/com/example/musinsapayments_test_project/MusinsapaymentsTestProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.musinsapayments_test_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusinsapaymentsTestProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusinsapaymentsTestProjectApplication.class, args);
+	}
+
+}
